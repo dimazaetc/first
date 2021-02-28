ccae9a0e21c3ebd5010ebd78ab4218ed1d27d70f
Dima <di,odi4@i.ua>
2021-02-28 14:01:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const answers = [];
+answers[0] = prompt('как ваше имя?', '');
+answers[1] = prompt('как вашф фамилия?', '');
+answers[2] = prompt('сколько вам лет?', '');
+
+document.write(answers);
